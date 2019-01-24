7c0211d93e577c66a07c62465fd277d0abda1692
febanda01 <febanda91@gmail.com>
2019-01-24 13:21:19 -0600

    update

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,23 +1,21 @@
+// Utilities
 let s = function(selector) {
-  return document.querySelector(selector);
-};
-let c = function(tagName) {
-  return document.createElement(tagName);
-};
+  return document.querySelector(selector)}
 
+let c = function(tagName) {
+  return document.createElement(tagName)}
+
+
+  // HTML Elements
 let workoutListContainer = s("#workout_collection");
 let workout_input = s("#workout_input");
 let workout_submit = s("#workout_submit");
 let delete_workout = s("#workout_delete");
-let workouts = [];
+let workouts = []
+let stats = []
+let exercises = []
 
-function fetchWorkouts() {
-  fetch("http://localhost:3000/api/v1/workouts")
-    .then(res => res.json())
-    .then(res => (workouts = res))
-    .then(render);
-}
-
+// Render
 function render() {
   workouts.forEach(workout => {
     let workoutItem = c("ol");
@@ -39,88 +37,20 @@ function render() {
   });
 }
 
-// function renderWorkout() {
-//   workoutList.innerText = "";
 
-//     workouts.forEach(
-//       workout => {
-//         workoutList.innerText = workout_input.value;
-//       }
-//       // workout_input.value = "";
-//       // workouts.forEach(workout => {
-//       // let workout = c("li");
-//       // });
-//     );
-//   });
-// }
-// render();
-// renderWorkout();
-fetchWorkouts();
 
-// let s = function(selector) {
-//     return document.querySelector(selector);
-//   };
-//   let c = function(tagName) {
-//     return document.createElement(tagName);
-//   };
-
-//   let workoutListContainer = s("#workout_collection");
-//   let workout_input = s("#workout_input");
-//   let workout_submit = s("#workout_submit");
-//   let delete_workout = s("#workout_delete");
-//   let workouts = [];
-
-//   function fetchWorkouts() {
-//     fetch("http://localhost:3000/api/v1/workouts")
-//       .then(res => res.json())
-//       .then(res => (workouts = res))
-//       .then(render);
-//   }
-
-//   function render() {
-//     workouts.forEach(workout => {
-//       let workoutItem = c("ol");
-//       let delete_workout = c("button");
-//       workoutItem.innerText = workout.name;
-//       workoutItem.setAttribute("class", "card");
-//       workoutItem.setAttribute("id", workoutItem.innerText);
-//       workoutItem.setAttribute("style", "font-size: 25px");
-//       workoutListContainer.append(workoutItem);
-//       workoutItem.append(delete_workout);
-//       delete_workout.innerText = "delete";
-//       delete_workout.setAttribute("id", "delete_workout");
-//     });
-
-//     const renderWorkout = () => {
-//       workout_submit.addEventListener("click", e => {
-//         e.preventDefault();
-
-//         let workoutItem = c("ol");
-//         workoutItem.innerText = workout_input.value;
-//         workoutListContainer.append(workoutItem);
-//       });
-//       //     fetch(`http://localhost:3000/workouts/${workouts.id}`, {
-//       //       method: "POST",
-//       //       headers: { "Content-Type": "application/json" },
-//       //       body: JSON.stringify(workouts)
-//       //     }).then(render);
-//       //   }
-//     };
-//   }
-//   // function renderWorkout() {
-//   //   workoutList.innerText = "";
-
-//   //     workouts.forEach(
-//   //       workout => {
-//   //         workoutList.innerText = workout_input.value;
-//   //       }
-//   //       // workout_input.value = "";
-//   //       // workouts.forEach(workout => {
-//   //       // let workout = c("li");
-//   //       // });
-//   //     );
-//   //   });
-//   // }
-//   render();
-//   renderWorkout();
-//   fetchWorkouts();
+// Fetch
+function fetchWorkouts() {
+  fetch("http://localhost:3000/api/v1/workouts")
+      .then(res => res.json())
+      .then(res => (workouts = res))
+      .then(
+        fetch("http://localhost:3000/api/v1/stats")
+          .then(res => res.json())
+          .then(res => (stats = res))
+    )
+    .then(fetch("http://localhost:3000/api/v1/exercises")
+      .then(res => res.json())
+      .then(res => (exercises = res))
+        .then(render))
+}
